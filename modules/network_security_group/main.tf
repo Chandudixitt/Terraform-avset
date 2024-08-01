b6b4a630e21a27721bde212579217c3f722cb23f
Chandudixitt <yelheriv@gmail.com>
2024-08-01 10:17:17 +0530

    newcommit

--- modules/network_security_group/main.tf
+++ modules/network_security_group/main.tf
@@ -37,7 +37,6 @@ resource "azurerm_network_security_group" "nsg" {
   tags = var.tags
 }
 
-
 resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
   count                      = length(var.subnet_ids)
   subnet_id                  = var.subnet_ids[count.index]
